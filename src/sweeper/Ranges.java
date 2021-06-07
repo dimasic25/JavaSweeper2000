52ca69683704bd2998baa84ea5444bd389025503
dimasic25 <78664325+dimasic25@users.noreply.github.com>
2021-06-07 16:25:37 +0300

    Update class Ranges (2)

--- src/sweeper/Ranges.java
+++ src/sweeper/Ranges.java
@@ -1,13 +1,24 @@
 package sweeper;
 
+import java.util.ArrayList;
+
 public class Ranges {
     private static Coord size;
+    private static ArrayList<Coord> allCoords;
 
     public static void setSize(Coord _size) {
         size = _size;
+        allCoords = new ArrayList<Coord>();
+        for (int y = 0; y < size.y; y++)
+            for (int x = 0; x < size.x; x++)
+                allCoords.add(new Coord(x, y));
     }
 
     public static Coord getSize() {
         return size;
+    }
+
+    public static ArrayList<Coord> getAllCoords() {
+        return allCoords;
     }
 }
