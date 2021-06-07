a89ac2282cd7e7cdc573e46eca92d7224194d38f
dimasic25 <78664325+dimasic25@users.noreply.github.com>
2021-06-07 16:57:52 +0300

    add class Matrix

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -19,6 +19,7 @@ public class JavaSweeper extends JFrame {
 
     private JavaSweeper() {
         game = new Game(COLS, ROWS);
+        game.start();
         setImages();
         initPanel();
         initFrame();
--- src/sweeper/Game.java
+++ src/sweeper/Game.java
@@ -1,11 +1,16 @@
 package sweeper;
 
 public class Game {
+    Matrix bombMap;
+
     public Game(int cols, int rows) {
         Ranges.setSize(new Coord(cols, rows));
     }
 
+    public void start() {
+        bombMap = new Matrix(Box.BOMB);
+    }
     public Box getBox(Coord coord) {
-        return Box.values()[(coord.x + coord.y) % Box.values().length];
+        return bombMap.get(coord);
     }
 }
